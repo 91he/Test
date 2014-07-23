04fd7e95bdc2d0672dc4310d4dc68b078052f933
levi <91he@sina.com>
2014-07-23 15:41:08 +0800

    thread pool

---
+++ pool/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "pool.h"
+
+void *handle(void *arg){
+	int x = rand() % 4 + 1;
+	printf("hello %d:%d\n", *((int *)arg), x);
+	sleep(x);
+}
+
+int main(){
+	srand(time(NULL));
+	struct thread_pool *pool = pool_init(4);
+	int i;
+	int num[10];
+	for(i = 0; i < 10; ++i)
+		num[i] = i;
+	for(i = 0; i < 10; ++i){
+		pool_add(pool, handle, num+i);
+	}
+	sleep(6);
+	pool_destroy(pool);
+}
